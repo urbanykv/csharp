2f01324ca26afde6cbf190e81c4d4ff3297fcd34
urbanykv <matheusurban19@gmail.com>
2023-10-27 21:22:56 -0300

    CSharp

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = 'Matheus';
+            string sobrenome = ' Urban';
+
+            Console.WriteLine(nome + sobrenome);
+        }
+    }
+}
