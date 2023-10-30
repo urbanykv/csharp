427846dc7a81b4c7021d828f9d95b249ceb970df
urbanykv <matheusurban19@gmail.com>
2023-10-30 07:41:27 -0300

    csharp

--- Program.cs
+++ Program.cs
@@ -7,6 +7,20 @@ Leandrinha.Nome = "Leandrinha";
 Leandrinha.Idade = 21;
 Leandrinha.Apresentar();
 
-DateTime dataAtual = DateTime.Now;
+DateTime dataAtual = DateTime.Now; // Data
 
-Console.WriteLine(dataAtual);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
+
+int a = 30;
+
+a += 5;
+
+a -= 5;
+
+a *= 5;
+
+a /= 5;
+
+int b = Convert. "5";
+
+Console.WriteLine(a);
