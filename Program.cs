65baa4288dec0bf042ee8ba47ed18f211a50e6cf
urbanykv <matheusurban19@gmail.com>
2023-11-24 14:13:12 -0300

    cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System.Globalization;
+using CSharp.Models;
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+Pessoa Aluno01 = new Pessoa("Matheus", "Urban", 21);
+
+Pessoa Aluno02 = new()
+{
+    Nome = "Débora",
+    Sobrenome = "Urban",
+    Idade = 19
+};
+
+Curso Ingles = new()
+{
+    Nome = "Inglês",
+    Alunos = new List<Pessoa>()
+};
+
+Ingles.AdicionarAluno(Aluno01);
+Ingles.AdicionarAluno(Aluno02);
+
+Ingles.ListarAlunos();
+
+decimal dolar = 2525.99M;
+
+Console.WriteLine($"{dolar:C0}");
+
+DateTime data = DateTime.Now;
+
+Console.WriteLine(data.ToString("dd/MM/yyyy"));
